55ef8d71583c9ee28b44832aa62ee0934860c50f
munmeow <hongbinhthai2003@gmail.com>
2022-12-07 13:28:38 +0700

    Add QuanLyDoanhThu

---
+++ DOAN/DOANHTHU/QuanLyDoanhThu.java
@@ -0,0 +1,19 @@
+package DOANHTHU;
+
+public class QuanLyDoanhThu{
+    public void NhapThongTin(){
+    }
+    public double SumOfBill(){
+        return 0;
+    }
+    public void money(){    
+    }
+    public void profit(){
+    }
+    public float tax(){
+        return 0;
+    }
+    public int usage(){
+        return 0;
+    }
+}
